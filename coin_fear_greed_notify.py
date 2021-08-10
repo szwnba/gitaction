55751201545d4e5fe4ba4b026356a3cec58692a7
szwnba <szwnba@qq.com>
2021-08-11 01:44:24 +0800

    687

--- coin_fear_greed_notify.py
+++ coin_fear_greed_notify.py
@@ -1,15 +1,11 @@
 #-*- encoding: utf-8 -*-
 #
-import sys
 
 import requests
 
+from app.BinanceAPI import BinanceAPI
+from app.dingding import Message
 from app.dingdingapi import senddingding_news
-
-sys.path.append('.')
-
-import json
-from urllib.request import urlopen
 
 
 def get_fear_greed():
@@ -18,12 +14,15 @@ def get_fear_greed():
 
 
 if __name__ == '__main__':
+    instance = BinanceAPI()
+    msg = Message()
     url = "https://api.alternative.me/fng/"
+    price = instance.get_ticker_price("BTCUSDT")
     result = requests.get(url).json()
     infos=[]
     for index in result["data"]:
        item = {}
-       item['title']=  index["value_classification"] + ": " + index["value"] + "\n\n> ![img](https://alternative.me/crypto/fear-and-greed-index.png)\n\n"
+       item['title']= "BTC =价格： {},".format(price) + index["value_classification"] + ": " + index["value"] + "\n\n> ![img](https://alternative.me/crypto/fear-and-greed-index.png)\n\n"
        item['link']="https://alternative.me/crypto/fear-and-greed-index/"
        item['subtype'] = "flow 市场情绪指数"
        infos.append(item)
