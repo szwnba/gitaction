8c0f746f07634aadcabfe3ce4b2c9b50b797fa74
szwnba <szwnba@qq.com>
2021-08-11 01:02:10 +0800

    123

---
+++ authorization.py
@@ -0,0 +1,8 @@
+# Get an Key and Secret
+# https://www.binance.com/restapipub.html
+
+api_key = ''
+api_secret = ''
+recv_window = 50000
+
+dingding_token = "fc1ad21323ce73728bafc3770f27763c36761abe87abe03642038b52e869f85f"
--- bitcoin.py
+++ bitcoin.py
@@ -1,2 +1,8 @@
+from BinanceAPI import BinanceAPI
 
-print('hello world')
+if __name__ == "__main__":
+    # instance = BinanceAPI(api_key,api_secret)
+    instance = BinanceAPI()
+    # print(instance.buy_limit("EOSUSDT",5,2))
+    # print(instance.get_ticker_price("WINGUSDT"))
+    print(instance.get_ticker_price("BTCUSDT"))
